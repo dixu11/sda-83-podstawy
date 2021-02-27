23b37c0ef78fe761845bca46a12fcb64b7e4be36
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-02-27 12:26:38 +0100

    formatowanie wyrażeń

--- src/literaly/LiteralyOmowienie.java
+++ src/literaly/LiteralyOmowienie.java
@@ -27,5 +27,15 @@ class LiteralyOmowienie {
 
 
         System.out.println(Math.pow(15,2));
+
+
+       // 10+20
+       // 7
+
+        System.out.println((10.0 + 20) / 7);
+
+        System.out.println("10 + 20 to " + (10+20) );
+        System.out.println((10+20) + "/ 7 daje " + (    10.0   + 20) / 7);
+
     }
 }
