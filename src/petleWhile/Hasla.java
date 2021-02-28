18599e759f6f6062a8b8d39d99174bc58e3555fd
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-02-28 13:52:43 +0100

    problem porownywania po tekscie

---
+++ src/petleWhile/Hasla.java
@@ -0,0 +1,16 @@
+package petleWhile;
+
+import java.util.Scanner;
+
+class Hasla {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String podaneHaslo;
+        do {
+            System.out.println("Podaj hasło");
+            podaneHaslo = scanner.nextLine();
+        } while (   !podaneHaslo.equals("abcd") );
+        System.out.println("Java jest spoko");
+
+    }
+}
