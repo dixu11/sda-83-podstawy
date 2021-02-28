4fb915675452e0233f70da50b59c0e5dd425604a
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-02-28 10:29:23 +0100

    rozbudowane warunki

--- src/instrukcjeWarunkowe/RozbudowaneWarunki.java
+++ src/instrukcjeWarunkowe/RozbudowaneWarunki.java
@@ -7,20 +7,20 @@ class RozbudowaneWarunki {
 
         // jeśli nie pada deszcz, i wróciłem z pracy to ide na spacer
 
-        boolean deszcz = true;
+        boolean deszcz = false;
         boolean wrocilemZPracy = false;
         boolean mamParasol = true;
         boolean mamyCheb = false;
 
 
-        if(deszcz == false &&  wrocilemZPracy == true ){
+        if (deszcz == false && wrocilemZPracy == true) {
             System.out.println("Ide na spacer");
-        }else {
+        } else {
             System.out.println("Zostaję w domu");
         }
 
         // jeśli nie pada deszcz lub mam parasol to idę na spacer
-        if(deszcz == false || mamParasol == true){
+        if (deszcz == false || mamParasol == true) {
             System.out.println("Idę na spacer");
         }
 
@@ -28,16 +28,26 @@ class RozbudowaneWarunki {
         //jeśli nie  i brakuje chleba to idę do sklepu
         //jeśli nie brakuje chleba to idę do kolegi
 
-        if(deszcz  || !mamParasol ){
+        if (deszcz || !mamParasol) {
             System.out.println("Oglądam telewizje");
-        }else if(! mamyCheb){
+        } else if (!mamyCheb) {
             System.out.println("Idę do sklepu po chleb");
-        }else {
+        } else {
             System.out.println("Idę do kolegi");
         }
 
 
+        // jutro nie będzie padać! zakładam się, jeśli przegram zakład dostajesz 10 zł
 
+        int kwotaWygrana;
+        if (deszcz) {
+            System.out.println("Wygrał zakład bo pada");
+            kwotaWygrana = 10;
+        } else {
+            kwotaWygrana = -10;
+            System.out.println("Przegrałem zakład bo nie pada");
+        }
 
+        System.out.println("Po zakładzie mam " + kwotaWygrana);
     }
 }
