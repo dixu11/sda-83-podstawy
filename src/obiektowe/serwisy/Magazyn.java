a99c9ee725f18472fb2fa970c21df0d3ab9bd608
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-03-27 10:23:13 +0100

    serwisy

--- src/obiektowe/serwisy/Magazyn.java
+++ src/obiektowe/serwisy/Magazyn.java
@@ -3,6 +3,19 @@ package obiektowe.serwisy;
 
 public class Magazyn {
 
+    public Produkt[] przygotujProdukty(){
+        Produkt produkt1 = new Produkt("kubek",3,13);
+        Produkt produkt2 = new Produkt("pizza",2,30);
+        Produkt produkt3 = new Produkt("cola",3,6);
+
+        Produkt[] towary = new Produkt[3];
+        towary[0] = produkt1;
+        towary[1] = produkt2;
+        towary[2] = produkt3;
+        return towary;
+    }
+
+
     public void doKtorychDarmowaWysylka(Produkt[] produkty){
         double darmowaWysylkaOd = 50; // darmowa wysylka od 50 zl
         for(Produkt produkt: produkty){
--- src/obiektowe/serwisy/MagazynDemo.java
+++ src/obiektowe/serwisy/MagazynDemo.java
@@ -2,25 +2,13 @@ package obiektowe.serwisy;
 
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MagazynDemo {
     public static void main(String[] args) {
-        Produkt produkt1 = new Produkt("kubek",3,13);
-        Produkt produkt2 = new Produkt("pizza",2,30);
-        Produkt produkt3 = new Produkt("cola",3,6);
-
-        Produkt[] towary = new Produkt[3];
-        towary[0] = produkt1;
-        towary[1] = produkt2;
-        towary[2] = produkt3;
-
-        System.out.println(Arrays.toString(towary));
-
         Magazyn magazyn = new Magazyn();
-        magazyn.doKtorychDarmowaWysylka(towary);
-
-
-
-
+        Produkt[] produkty =   magazyn.przygotujProdukty();
+        System.out.println(Arrays.toString(produkty));
+        magazyn.doKtorychDarmowaWysylka(produkty);
     }
 }
